5335418f55d061c1cf4c637b28d4e9c82a507dd6
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-25 14:28:04 -0500

    We do a desestruture of useMayus and we create a 3 buttons, the first one to put mayus the second to put minus and the third to add texts

--- src/components/PruebaCustom.js
+++ src/components/PruebaCustom.js
@@ -3,18 +3,25 @@ import { useMayus } from '../hooks/useMayus'
 
 export const PruebaCustom = () => {
 
+    const {estado, mayusculas, minusculas, concatenar} = useMayus("Soy el mejor programador del mundo");
+
     let nombre = ", Me llamo Carlos Villegas Chirinos";
 
-    const {mayusculas, minusculas, concatenar} = useMayus("Soy el mejor programador del mundo");
-    console.log(mayusculas());
-    console.log(minusculas());
-    console.log(concatenar(nombre));
-    console.log(minusculas(nombre));
-    console.log(mayusculas(nombre));
+    // console.log(mayusculas());
+    // console.log(minusculas());
+    // console.log(concatenar(nombre));
+    // console.log(minusculas(nombre));
+    // console.log(mayusculas(nombre));
 
   return (
     <div>
         <h1>Probando Componentes Personalizados</h1>
+            <h2>{estado}</h2>
+
+            <p><button onClick={ mayusculas }>Poner en MAYÚSCULAS</button></p>
+            <p><button onClick={ minusculas }>Poner en minúsculas</button></p>
+            <p><button onClick={ e => concatenar(nombre) }>Concatenar textos</button></p>
+
     </div>
   )
 }
