4310ef46a9be62eb9f9aa78ea6d30fb627dff306
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-25 13:40:33 -0500

    Import PruebaCustom component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { MiComponente } from './components/MiComponente';
+import { PruebaCustom } from './components/PruebaCustom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MiComponente />
+        <PruebaCustom />
       </header>
     </div>
   );
