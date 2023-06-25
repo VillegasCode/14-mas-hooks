3ed7a7240348ad8a4e0ded29a06b3130ce99a776
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-25 14:29:40 -0500

    We create a hooks useState with miTexto, setMiTexto and return estado: miTexto

--- src/hooks/useMayus.js
+++ src/hooks/useMayus.js
@@ -1,21 +1,52 @@
+import { useState } from "react";
+
+
 export const useMayus = (texto) => {
-    const mayusculas = (valor = "") => {
-        return texto.toUpperCase() + valor.toUpperCase();
+
+    const [miTexto, setMiTexto] = useState(texto);
+
+    const mayusculas = () => {
+        setMiTexto(texto.toUpperCase());
     }
 
-    const minusculas = (valor = "") => {
-        let txt1 = texto.toLowerCase();
-        let txt2 = valor.toLowerCase();
-        return txt1+txt2;
+    const minusculas = () => {
+        setMiTexto(texto.toLowerCase());
     }
 
     const concatenar = (agregado) => {
-        return texto+agregado;
+        setMiTexto(texto+agregado);
     }
 
     return {
+        estado: miTexto,
         mayusculas,
         minusculas,
         concatenar
     };
+
+
+
+
+
+
+    //Usando el (valor = "") como parámetro opcional
+    // const mayusculas = (valor = "") => {
+    //     return texto.toUpperCase() + valor.toUpperCase();
+    // }
+
+    // const minusculas = (valor = "") => {
+    //     let txt1 = texto.toLowerCase();
+    //     let txt2 = valor.toLowerCase();
+    //     return txt1+txt2;
+    // }
+
+    // const concatenar = (agregado) => {
+    //     return texto+agregado;
+    // }
+
+    // return {
+    //     mayusculas,
+    //     minusculas,
+    //     concatenar
+    // };
 }
