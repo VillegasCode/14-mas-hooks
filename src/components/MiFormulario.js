984db0e367297b6e4f5ab2df212b2f89604acaa7
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-27 12:24:18 -0500

    Capture e.target of the form

--- src/components/MiFormulario.js
+++ src/components/MiFormulario.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const MiFormulario = () => {
+
+  const [formulario, setFormulario] = useState({});
+
+  const enviado = (e) =>{
+      e.preventDefault();
+      
+      let curso = {
+        titulo: e.target.titulo.value,
+        anio: e.target.anio.value,
+        descripcion: e.target.descripcion.value,
+        autor: e.target.autor.value,
+        email: e.target.email.value,
+      }
+
+      setFormulario(curso);
+  }
+
   return (
     <div>
         <h1>FORMULARIO</h1>
         <p>Formulario para guardar curso</p>
         <p>Curso guardado:</p>
+        <pre>{JSON.stringify(formulario)}</pre>
 
-        <form className='mi-formulario'>
+        <form onSubmit={enviado} className='mi-formulario'>
             <input type='text' name='titulo' placeholder='Título:' />
             <input type='number' name='anio' placeholder='Año publicación:' />
             <textarea name='descripcion' placeholder='Descripción:' />
