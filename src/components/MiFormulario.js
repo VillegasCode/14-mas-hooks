6f5670ac24f4358f221eb1ab02cd45087ece1b4a
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-26 21:26:41 -0500

    Creation of form to save courses

--- src/components/MiFormulario.js
+++ src/components/MiFormulario.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export const MiFormulario = () => {
   return (
-    <div>MiFormulario</div>
+    <div>
+        <h1>FORMULARIO</h1>
+        <p>Formulario para guardar curso</p>
+        <p>Curso guardado:</p>
+
+        <form className='mi-formulario'>
+            <input type='text' name='titulo' placeholder='Título:' />
+            <input type='number' name='anio' placeholder='Año publicación:' />
+            <textarea name='descripcion' placeholder='Descripción:' />
+            <input type='text' name='autor' placeholder='Autor:' />
+            <input type='email' name='email' placeholder='Correo de contacto:' />
+
+            <input type='submit' value="Enviar" />
+        </form>
+    </div>
   )
 }
