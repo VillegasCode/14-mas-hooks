474ab98c288c31401dbf1c6b92e80cc362dfc216
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-27 11:56:47 -0500

    MiFormulario component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { MiComponente } from './components/MiComponente';
 import { PruebaCustom } from './components/PruebaCustom';
+import { MiFormulario } from './components/MiFormulario';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MiComponente />
-        <PruebaCustom />
+        {/* <MiComponente /
+        <PruebaCustom /> */}
+        <MiFormulario />
       </header>
     </div>
   );
