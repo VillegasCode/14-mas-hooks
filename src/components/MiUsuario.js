3d83d291865e2ede9a02ef05d0ff57ec4c6d7980
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-28 18:57:13 -0500

    Hook useState with destructure url, setUrl and we load datos, cargando and url to the useAjax

--- src/components/MiUsuario.js
+++ src/components/MiUsuario.js
@@ -1,47 +1,66 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { useAjax } from '../hooks/useAjax'
 
 export const MiUsuario = () => {
-  
-    const [usuario, setUsuario] = useState({
-        datos: null,
-        cargando: true
-    });
 
-    const getUsuario = async(url) => {
-        setUsuario({
-            ...usuario,
-            cargando: true
-        });
-        
-        const peticion = await fetch(url);
-        const {data} = await peticion.json();
+    const [url, setUrl] = useState("https://reqres.in/api/users/1");
+    const {datos, cargando} = useAjax(url);
 
-        console.log(data);
-        setUsuario({
-            datos: data,
-            cargando: false
-        });
-
-    }
-  
     const getId = e => {
         let id = parseInt(e.target.value);
-        let url = "https://reqres.in/api/users/" + id;
-        getUsuario(url);
+        setUrl("https://reqres.in/api/users/" + id);
     }
-  
 
-    useEffect(() => {
-        getUsuario("https://reqres.in/api/users/1");
-    }, [])
+    //Esto que está comentado se pasó a un hook aparte llamado useAjax.js
+    // const getId = e => {
+    //     let id = parseInt(e.target.value);
+    //     let url = "https://reqres.in/api/users/" + id;
+    //     getUsuario(url);
+    // }
+    
+    // const [usuario, setUsuario] = useState({
+    //     datos: null,
+    //     cargando: true
+    // });
+
+    // const getUsuario = async(url) => {
+    //     setUsuario({
+    //         ...usuario,
+    //         cargando: true
+    //     });
+        
+    //     const peticion = await fetch(url);
+    //     const {data} = await peticion.json();
+
+    //     console.log(data);
+    //     setUsuario({
+    //         datos: data,
+    //         cargando: false
+    //     });
+
+    // }  
+
+    // useEffect(() => {
+    //     getUsuario("https://reqres.in/api/users/1");
+    // }, [])
 
     return (
-    <div>
+
+        <div>
         <h1>Mi Usuario:</h1>
         <p>Datos del usuario</p>
-        <p>{ usuario.cargando ? "Cargando..." : ""}</p>
-        <p>{usuario?.datos?.first_name} {usuario?.datos?.last_name}</p>
+        <p>{ cargando ? "Cargando..." : ""}</p>
+        <p>{ datos?.first_name} {datos?.last_name}</p>
         <input type='number' name='id' onChange={getId} />
     </div>
+
+
+    // <div>
+    //     <h1>Mi Usuario:</h1>
+    //     <p>Datos del usuario</p>
+    //     <p>{ usuario.cargando ? "Cargando..." : ""}</p>
+    //     <p>{usuario?.datos?.first_name} {usuario?.datos?.last_name}</p>
+    //     <input type='number' name='id' onChange={getId} />
+    // </div>
   )
 }
