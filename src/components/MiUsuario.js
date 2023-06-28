4bae5b203d910e4b3b5f9e8f4b4e8561fcf53397
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-28 14:41:26 -0500

    useEffect hook added + cargando conditional

--- src/components/MiUsuario.js
+++ src/components/MiUsuario.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const MiUsuario = () => {
   
     const [usuario, setUsuario] = useState({
-        datos: null
+        datos: null,
+        cargando: true
     });
 
     const getUsuario = async(url) => {
+        setUsuario({
+            ...usuario,
+            cargando: true
+        });
+        
         const peticion = await fetch(url);
         const {data} = await peticion.json();
 
         console.log(data);
         setUsuario({
-            datos: data
+            datos: data,
+            cargando: false
         });
 
     }
@@ -23,11 +30,17 @@ export const MiUsuario = () => {
         getUsuario(url);
     }
   
+
+    useEffect(() => {
+        getUsuario("https://reqres.in/api/users/1");
+    }, [])
+
     return (
     <div>
         <h1>Mi Usuario:</h1>
         <p>Datos del usuario</p>
-        <p>{usuario?.datos?.first_name}</p>
+        <p>{ usuario.cargando ? "Cargando..." : ""}</p>
+        <p>{usuario?.datos?.first_name} {usuario?.datos?.last_name}</p>
         <input type='number' name='id' onChange={getId} />
     </div>
   )
